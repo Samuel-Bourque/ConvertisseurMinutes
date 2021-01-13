2e3d4e9df9785640e730bd9f7f93ffcbef31fbd0
Samuel Bourque <70170253+Samuel-Bourque@users.noreply.github.com>
2021-01-13 00:38:58 -0500

    Add files via upload

---
+++ labo2-minutes-Samuel-Bourque/Calculateur_Affaires/Calculateur.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Calculateur_Affaires
+{
+    public class Calculateur
+    {
+        public double calculerMinutes(int nbSecondes) {
+            return (double) nbSecondes / 60;
+        }
+    }
+}
---
+++ labo2-minutes-Samuel-Bourque/Labo2Minutes_UI/ConvertisseurIntDouble.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Data;
+
+namespace Labo2Minutes_UI
+{
+    [ValueConversion(typeof(string), typeof(int))]
+    public class ConvertisseurIntDouble : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            return value.ToString();
+        }
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            int num;
+            string chaine = value as string;
+            if (int.TryParse(chaine, out num) && !chaine.EndsWith("."))
+            {
+                return num;
+            }
+            return DependencyProperty.UnsetValue;
+        }
+    }
+}
